183d25ae5ba9204dce9774aa8a735441c4f39d2b
Ariel Arévalo Alvarado <ariel.arevalo@ucr.ac.cr>
2023-09-23 21:06:51 -0600

    AS-65: Agrega estructura de capas del proyecto

--- Locompro/Models/User.cs
+++ Locompro/Models/User.cs
@@ -2,8 +2,10 @@
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography.X509Certificates;
 
-namespace Locompro.Models {
-    public class User {
+namespace Locompro.Models
+{
+    public class User
+    {
         [Key]
         [Required]
         public string Username { get; set; }
