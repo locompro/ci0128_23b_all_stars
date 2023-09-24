052450533b9739c0586ff66987c2930675c78b52
Brandon Mora Umaña <brandon.m.u@hotmail.com>
2023-09-24 15:01:15 -0600

    Añade una implementación inicial del repositorio User y servicio User

---
+++ Locompro/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Locompro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locompro.Repositories
+{
+    public class UserRepository : AbstractRepository<User, string>
+    {
+        public UserRepository(DbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
