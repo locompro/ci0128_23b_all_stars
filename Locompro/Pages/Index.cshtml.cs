b86e64cacc54ce779f1f2d3eb6c26e32b2a58b12
Joseph Stuart Valverde Kong <joseph.valverdekong@ucr.ac.cr>
2023-09-25 21:41:32 -0600

    Se simplifica el codigo eliminando necesidad de programacion en el modelo y se clarifica la funcionalidad al dirigir a dos paginas de busqueda diferente para las busquedas

--- Locompro/Pages/Index.cshtml.cs
+++ Locompro/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ namespace locompro.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+            
         public string SearchQuery { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,44 +14,14 @@ namespace locompro.Pages
             _logger = logger;
         }
 
-        public void OnGet(string SearchString)
+        public void OnGet()
         {
 
         }
 
-        public void OnPost(string SearchString)
+        public void OnPost()
         {
-            // Get the button that was clicked
-            string ButtonClicked = Request.Form["SearchButton"];
 
-            const string searchLink = "Error"; // TODO: change this to the search page
-
-            // If a button was clicked
-            if (ButtonClicked != null)
-            {
-                // Redirect to the appropriate page
-                switch (ButtonClicked)
-                {
-                    // for normal searches
-                    case "normalSearchButton":
-                        Response.Redirect(searchLink + "?normalSearch=" + SearchString);
-                        break;
-                    // for advanced searches
-                    case "advancedSearchButton":
-                        Response.Redirect(searchLink + "?advancedSearch=" + SearchString);
-                        break;
-                    // for any other kind of search type given which has not been defined
-                    default:
-                        // do nothing and return
-                        Response.Redirect("./Index");
-                        break;
-                }
-            }
-            else
-            {
-                // if button was not clicked, then redirect back
-                Response.Redirect("./Index");
-            }
         }
     }
 }
