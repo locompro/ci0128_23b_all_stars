a0904761aae5abae2377ceb79810f803f7c65669
Joseph Stuart Valverde Kong <joseph.valverdekong@ucr.ac.cr>
2023-09-25 19:43:01 -0600

    Fixed common layout incompatabilities

--- Locompro/Pages/Error.cshtml.cs
+++ Locompro/Pages/Error.cshtml.cs
@@ -14,8 +14,6 @@ namespace locompro.Pages
 
         private readonly ILogger<ErrorModel> _logger;
 
-        public string text { get; set; }
-
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
@@ -23,23 +21,7 @@ namespace locompro.Pages
 
         public void OnGet()
         {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-
-            // from here on out example on how use the parameters for normal and advanced search
-            string searchString = Request.Query["normalSearch"];
-
-            if (searchString == null)
-            {
-                this.text = "advanced search: ";
-                searchString = Request.Query["advancedSearch"];
-            } else
-            {
-                this.text = "normal search: ";
-            }
-
-            this.text += searchString;
-
-            Console.WriteLine(text);
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;        
         }
     }
 }
