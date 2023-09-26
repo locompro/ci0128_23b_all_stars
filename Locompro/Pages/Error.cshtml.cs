6261ab83067d9a7b6950867bbadd1a22830f9026
Joseph Stuart Valverde Kong <joseph.valverdekong@ucr.ac.cr>
2023-09-25 18:07:06 -0600

    Cambios y refinamientos de pantalla principal y desacoplado de lenguages por documento

--- Locompro/Pages/Error.cshtml.cs
+++ Locompro/Pages/Error.cshtml.cs
@@ -14,6 +14,8 @@ namespace locompro.Pages
 
         private readonly ILogger<ErrorModel> _logger;
 
+        public string text { get; set; }
+
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
@@ -22,6 +24,22 @@ namespace locompro.Pages
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+            // from here on out example on how use the parameters for normal and advanced search
+            string searchString = Request.Query["normalSearch"];
+
+            if (searchString == null)
+            {
+                this.text = "advanced search: ";
+                searchString = Request.Query["advancedSearch"];
+            } else
+            {
+                this.text = "normal search: ";
+            }
+
+            this.text += searchString;
+
+            Console.WriteLine(text);
         }
     }
 }
--- Locompro/Pages/Index.cshtml.cs
+++ Locompro/Pages/Index.cshtml.cs
@@ -7,14 +7,51 @@ namespace locompro.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public string SearchQuery { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(string SearchString)
         {
 
+        }
+
+        public void OnPost(string SearchString)
+        {
+            // Get the button that was clicked
+            string ButtonClicked = Request.Form["SearchButton"];
+
+            const string searchLink = "Error"; // TODO: change this to the search page
+
+            // If a button was clicked
+            if (ButtonClicked != null)
+            {
+                // Redirect to the appropriate page
+                switch (ButtonClicked)
+                {
+                    // for normal searches
+                    case "normalSearchButton":
+                        Response.Redirect(searchLink + "?normalSearch=" + SearchString);
+                        break;
+                    // for advanced searches
+                    case "advancedSearchButton":
+                        Response.Redirect(searchLink + "?advancedSearch=" + SearchString);
+                        break;
+                    // for any other kind of search type given which has not been defined
+                    default:
+                        // do nothing and return
+                        Response.Redirect("./Index");
+                        break;
+                }
+            }
+            else
+            {
+                // if button was not clicked, then redirect back
+                Response.Redirect("./Index");
+            }
         }
     }
 }
