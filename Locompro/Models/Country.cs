43abf40ed4b51942befd30e7f30e77959e339f49
Gabriel Molina Bulgarelli <gabriel.molinabulgarelli@ucr.ac.cr>
2023-09-29 04:54:23 -0600

    AS-18: Agrega modelos para ubicar tienda

---
+++ Locompro/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Locompro.Models
+{
+    /// <summary>
+    /// A country with provinces.
+    /// </summary>
+    public class Country
+    {
+        [Key]
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Province> Provinces { get; set; }
+    }
+}
