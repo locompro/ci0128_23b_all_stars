fa005c3f7baa20756e032c1125b561119027222c
Gabriel Molina Bulgarelli <gabriel.molinabulgarelli@ucr.ac.cr>
2023-09-30 07:20:11 -0600

    AS-18: Agrega modelos y contexto para estructurar contribución

---
+++ Locompro/Models/Product.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Locompro.Models;
+
+public class Product
+{
+    [Key] [Required] public int Id { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 1)]
+    public string Name { get; set; }
+
+    [StringLength(60)] public string Model { get; set; }
+
+    [StringLength(60)] public string Brand { get; set; }
+    
+    [Required]
+    public Status Status { get; set; } = Status.Active;
+
+    public virtual ICollection<Category> Categories { get; set; }
+    
+    public virtual ICollection<Category> Submissions { get; set; }
+
+}
