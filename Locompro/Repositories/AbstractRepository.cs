3a807666b7481751b4c77fe1391124b418ab3762
Gabriel Molina Bulgarelli <gabriel.molinabulgarelli@ucr.ac.cr>
2023-09-30 17:03:43 -0600

    AS-18: Agrega preparación de configuración para utilizar repositorios

---
+++ Locompro/Repositories/AbstractRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Locompro.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Locompro.Repositories
+{
+    /// <summary>
+    /// Generic repository for an application entity type.
+    /// </summary>
+    /// <typeparam name="T">Type of entity handled by repository.</typeparam>
+    /// <typeparam name="I">Type of key used by entity.</typeparam>
+    public abstract class AbstractRepository<T, I> : IRepository<T, I> where T: class
+    {
+        protected readonly ILogger Logger;
+        protected readonly LocomproContext Context;
+        protected readonly DbSet<T> DbSet;
+
+        /// <summary>
+        /// Constructs a repository for a given context.
+        /// </summary>
+        /// <param name="context">Context to base the repository on.</param>
+        /// <param name="loggerFactory">Factory for repository logger.</param>
+        protected AbstractRepository(LocomproContext context, ILoggerFactory loggerFactory)
+        {
+            Logger = loggerFactory.CreateLogger(GetType());
+            Context = context;
+            DbSet = Context.Set<T>();
+        }
+
+        /// <inheritdoc />
+        public async Task<T> GetByIdAsync(I id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task AddAsync(T entity)
+        {
+            await DbSet.AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task UpdateAsync(T entity)
+        {
+            DbSet.Update(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task DeleteAsync(I id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                DbSet.Remove(entity);
+                await Context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Locompro/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Locompro.Repositories
+{
+    public interface IRepository<T, I>
+    {
+        Task<T> GetByIdAsync(I id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(I id);
+    }
+}
