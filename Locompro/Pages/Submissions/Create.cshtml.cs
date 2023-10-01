e110325cf7247960da55904c898f86489c0e5286
Ariel Arévalo Alvarado <ariel.arevalo@ucr.ac.cr>
2023-09-30 23:44:02 -0600

    AS-65: Cambia la pantalla de agregar tienda a vista parcial.

--- Locompro/Pages/Submissions/Create.cshtml.cs
+++ Locompro/Pages/Submissions/Create.cshtml.cs
@@ -1,15 +1,17 @@
 using Locompro.Models;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Locompro.Pages.Submissions;
 
 public class CreateModel : PageModel
 {
+    [BindProperty]
     public StoreViewModel StoreVm { get; set; }
     
     public void OnGet()
     {
-
     }
 
     // public PartialViewResult OnGetCreateStore(string customerId) => Partial("Components/Stores/Create");
