06e2038b0148e741934c9428948dd0c222891016
Ariel Arévalo Alvarado <ariel.arevalo@ucr.ac.cr>
2023-10-01 22:34:03 -0600

    AS-65: Agrega entidades faltantes a DB context

--- Locompro/Data/LocomproContext.cs
+++ Locompro/Data/LocomproContext.cs
@@ -2,39 +2,40 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Locompro.Data
+namespace Locompro.Data;
+
+public class LocomproContext : IdentityDbContext<User>
 {
-    public class LocomproContext : IdentityDbContext<User>
+    public DbSet<Store> Store { get; set; } = default!;
+    public DbSet<Country> Countries { get; set; } = default!;
+    public DbSet<Province> Provinces { get; set; }
+    public DbSet<Canton> Cantons { get; set; }
+
+    public LocomproContext(DbContextOptions<LocomproContext> options)
+        : base(options)
     {
-        public DbSet<Province> Provinces { get; set; }
-        public DbSet<Canton> Cantons { get; set; }
-        
-        public LocomproContext(DbContextOptions<LocomproContext> options)
-            : base(options)
-        {
-        }
-        
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            
-            // Province
-            modelBuilder.Entity<Province>()
-                .HasKey(p => new { p.CountryName, p.Name });
+    }
 
-            modelBuilder.Entity<Province>()
-                .HasOne(p => p.Country)
-                .WithMany(c => c.Provinces)
-                .HasForeignKey(p => p.CountryName);
-            
-            // Canton
-            modelBuilder.Entity<Canton>()
-                .HasKey(c => new { c.CountryName, c.ProvinceName, c.Name });
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
 
-            modelBuilder.Entity<Canton>()
-                .HasOne(c => c.Province)
-                .WithMany(p => p.Cantons)
-                .HasForeignKey(c => new { c.CountryName, c.ProvinceName });
-        }
+        // Province
+        builder.Entity<Province>()
+            .HasKey(p => new { p.CountryName, p.Name });
+
+        builder.Entity<Province>()
+            .HasOne(p => p.Country)
+            .WithMany(c => c.Provinces)
+            .HasForeignKey(p => p.CountryName);
+
+        // Canton
+        builder.Entity<Canton>()
+            .HasKey(c => new { c.CountryName, c.ProvinceName, c.Name });
+
+        builder.Entity<Canton>()
+            .HasOne(c => c.Province)
+            .WithMany(p => p.Cantons)
+            .HasForeignKey(c => new { c.CountryName, c.ProvinceName });
     }
 }
