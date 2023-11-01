6718d53485d8734577d994d3e62bdd6a2c5bd790
Brandon Alonso Mora Umaña <brandon.m.u@hotmail.com>
2023-11-01 17:56:18 -0600

    AS-14: Empieza implementación de búsqueda por categoria como autocompletado

---
+++ Source/Locompro/wwwroot/js/categoryAutocomplete.js
@@ -0,0 +1,37 @@
+﻿import {ModalManager} from "./modal-manager.js"
+
+const select2SpanishLanguageSettings = {
+    errorLoading: function () {
+        return 'No se pudieron cargar los resultados.';
+    },
+    inputTooLong: function (args) {
+        var remainingChars = args.input.length - args.maximum;
+        return 'Por favor, elimine ' + remainingChars + ' caracter(es)';
+    },
+    inputTooShort: function (args) {
+        var remainingChars = args.minimum - args.input.length;
+        return 'Por favor, introduzca ' + remainingChars + ' o más caracteres';
+    },
+    loadingMore: function () {
+        return 'Cargando más resultados...';
+    },
+    maximumSelected: function (args) {
+        return 'Sólo puede seleccionar ' + args.maximum + ' elemento(s)';
+    },
+    noResults: function () {
+        return 'No se encontraron resultados';
+    },
+    searching: function () {
+        return 'Buscando...';
+    }
+};
+
+$(document).ready(function () {
+
+    $('#categoryName').select2({ 
+        placeholder: 'Seleccionar',
+        minimumInputLength: 1,
+        language: select2SpanishLanguageSettings,
+    });
+
+});
