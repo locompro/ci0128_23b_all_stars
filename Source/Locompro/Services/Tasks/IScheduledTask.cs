78e7f9345c239a04db046e648e90fda3fb3e7013
Brandon Alonso Mora Umaña <brandon.m.u@hotmail.com>
2023-11-02 21:01:57 -0600

    AS-76: Agrega cimientos para la construcción de servicios cron

---
+++ Source/Locompro/Services/Tasks/IScheduledTask.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace Locompro.Services.Tasks;
+
+/// <summary>
+/// Interface for a task that runs in the background.
+/// </summary>
+public interface IScheduledTask
+{
+    /// <summary>
+    /// Identifies how often the task should run.
+    /// Format: [Days,] Hours, Minutes, Seconds
+    /// </summary>
+    TimeSpan Interval { get; }
+
+
+    Task ExecuteAsync(CancellationToken cancellationToken);
+}
