5238469c4396043f6e38188c15a8b7e68bb34563
Brandon Alonso Mora Umaña <brandon.m.u@hotmail.com>
2023-11-05 01:07:28 -0600

    AS-76: Agrega una clase estática para el nombre de los roles

---
+++ Source/Locompro/Common/RoleNames.cs
@@ -0,0 +1,17 @@
+﻿namespace Locompro.Common;
+
+/// <summary>
+/// Static class containing constants for role names used within the application.
+/// </summary>
+public static class RoleNames
+{
+    /// <summary>
+    /// The role name for users who are moderators.
+    /// </summary>
+    public const string Moderator = "Moderator";
+
+    /// <summary>
+    /// The role name for users who are potential moderators.
+    /// </summary>
+    public const string PossibleModerator = "PossibleModerator";
+}
