ad80ef6e7fbc4a306d58a72906082905697e5cd0
Brandon Mora Umaña <brandon.m.u@hotmail.com>
2023-11-28 00:54:42 -0600

    AS-321: Crea indice para moderación. Agrega un nuevo estado para un submission anómalo

---
+++ Source/Locompro/Pages/Moderation/ModerationIndex.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Locompro.Pages.Moderation;
+
+public class ModerationIndex : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
