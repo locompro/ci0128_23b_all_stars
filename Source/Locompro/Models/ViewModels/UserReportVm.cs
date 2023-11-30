ceb89a59c24777be6a784edd9ed07de157bd1529
Brandon Alonso Mora Umaña <brandon.m.u@hotmail.com>
2023-11-30 01:25:33 -0600

    AS-321: Agrega Vm para los nuevos tipos de reporte. Cambia los nombres para mejor lectura
    Signed-off-by: Brandon Alonso Mora Umaña <brandon.m.u@hotmail.com>

---
+++ Source/Locompro/Models/ViewModels/UserReportVm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Locompro.Models.ViewModels;
+
+public class UserReportVm
+{
+    [Required] public string SubmissionUserId { get; set; }
+
+    [Required] public DateTime SubmissionEntryTime { get; set; }
+
+    [Required] public string UserId { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return $"UserReportVm: SubmissionUserId={SubmissionUserId}, " +
+               $"SubmissionEntryTime={SubmissionEntryTime}, " +
+               $"UserId={UserId}";
+    }
+}
